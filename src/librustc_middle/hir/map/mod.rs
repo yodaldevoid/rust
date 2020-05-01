938a400d78daa29dc91c4797dcb2f85b4817e3c9
Bastian Kauschke <bastian_kauschke@hotmail.de>
2020-05-01 16:04:55 +0200

    add fn enclosing_body_owner to hir

--- src/librustc_middle/hir/map/mod.rs
+++ src/librustc_middle/hir/map/mod.rs
@@ -374,6 +374,16 @@ impl<'hir> Map<'hir> {
         }
     }
 
+    pub fn enclosing_body_owner(&self, hir_id: HirId) -> HirId {
+        for (parent, _) in self.parent_iter(hir_id) {
+            if let Some(body) = self.maybe_body_owned_by(parent) {
+                return self.body_owner(body);
+            }
+        }
+
+        bug!("no `enclosing_body_owner` for hir_id `{}`", hir_id);
+    }
+
     /// Returns the `HirId` that corresponds to the definition of
     /// which this is the body of, i.e., a `fn`, `const` or `static`
     /// item (possibly associated), a closure, or a `hir::AnonConst`.
